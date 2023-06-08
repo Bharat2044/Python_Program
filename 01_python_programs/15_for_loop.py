ebc5464f15f7c626a254bdc832a5fa3cd741ca73
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:40 +0000

    code added

---
+++ 01_python_programs/15_for_loop.py
@@ -0,0 +1,37 @@
+n = 4;
+for i in range(0, n):
+    print(i);
+    
+print();
+
+print("List Iteration")
+l = ["Raj", "Kumar", "Singh"];
+for i in l:
+    print(i);
+    
+print();
+    
+print("\nTuple Iteration");
+t = ("Raj", "Kumar", "Singh");
+for i in t:
+    print(i);
+    
+print();
+
+print("\nString Iteration");
+s = "Bharat";
+for i in s:
+    print(i);
+    
+print();
+
+list = ["Raj", "Kumar", "Singh"];
+for index in range(len(list)):
+    print(list[index]);
+else:
+    print("Inside Else Block");
+    
+    
+    
+    
+    
