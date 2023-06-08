53449be3648388432cee723774667a351686255d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:18 +0000

    code added

---
+++ 01_python_programs/10_assignment_operators.py
@@ -0,0 +1,65 @@
+# Assignment Operator
+a = 3;
+b = 5;
+c = a + b;
+print(c);
+
+a = 3;
+b = 5;
+a += b;
+print(a);
+
+a = 3;
+b = 5;
+a -= b;
+print(a);
+
+a = 3;
+b = 5;
+a *= b;
+print(a);
+
+a = 3;
+b = 5;
+a /= b;
+print(a);
+
+a = 3;
+b = 5;
+a %= b;
+print(a);
+
+a = 3;
+b = 5;
+a //= b;
+print(a);
+
+a = 3;
+b = 5;
+a **= b;
+print(a);
+
+a = 3;
+b = 5;
+a &= b;
+print(a);
+
+a = 3;
+b = 5;
+a |= b;
+print(a);
+
+a = 3;
+b = 5;
+a ^= b;
+print(a)
+
+a = 3;
+b = 5;
+a >>= b;
+print(a);
+
+a = 3;
+b = 5;
+a <<= b;
+print(a);
