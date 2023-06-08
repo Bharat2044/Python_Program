ad183582b58087eb660c8eb09812301034c0b84c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:15 +0000

    code added

---
+++ 01_python_programs/08_logical_operators.py
@@ -0,0 +1,26 @@
+a = 10;
+b = 10;
+c = -10;
+
+print(True and True);
+print(True and False);
+print(False and True);
+print(False and False);
+
+print();
+
+print(True or True);
+print(True or False);
+print(False or True);
+print(False or False);
+
+print();
+
+print(not True);
+print(not False);
+
+print();
+
+print(a>=b and a<b);
+print(a>=b or a<b);
+print(not(a>=b and a<b));
