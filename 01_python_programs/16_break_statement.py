cf8bcf63d6392ed1927da9e3c83ad9221909f272
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:48 +0000

    code added

---
+++ 01_python_programs/16_break_statement.py
@@ -0,0 +1,13 @@
+fruits1 = ["apple", "orange", "banana", "cherry"];
+for x in fruits1:
+    print(x);
+    if x == "banana":
+        break;
+  
+print();
+
+fruits2 = ["apple", "orange", "banana", "cherry"];
+for x in fruits2:
+    if x == "banana":
+        break;
+    print(x);
