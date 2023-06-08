3170db29a716b5c8e933ec370e9331563be7ebd3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:12 +0000

    code added

---
+++ 01_python_programs/09_bitwise_operators.py
@@ -0,0 +1,28 @@
+a = 10;
+b = 4;
+ 
+# Print bitwise AND operation
+print("a & b =", a & b);
+print();
+ 
+# Print bitwise OR operation
+print("a | b =", a | b);
+print();
+ 
+# Print bitwise NOT operation
+print("~a =", ~a);
+print();
+ 
+# print bitwise XOR operation
+print("a ^ b =", a ^ b);
+print();
+
+# print bitwise right shift operator
+print("a >> 1 =", a >> 1);
+print("b >> 1 =", b >> 1);
+print();
+ 
+# print bitwise left shift operator
+print("a << 1 =", a << 1);
+print("b << 1 =", b << 1);
+ 
