bf0d4988a05c6e8ce0208c293ed16a710aa80658
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:52:28 +0000

    code added

---
+++ 01_python_programs/29_class_and_objects.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__ (self, name, roll):
+        self.name = name;
+        self.roll = roll;
+
+s1 = Student('Bharat', 101);
+s2 = Student('Raju', 190);
+        
+print(s1.name);
+print(s2.roll);
