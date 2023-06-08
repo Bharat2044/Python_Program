619425d866c2ee17153fee5d96c46211b7428d12
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:27 +0000

    code added

---
+++ 01_python_programs/12_type_casting.py
@@ -0,0 +1,39 @@
+a = 12;
+print(a, type(a));
+a = float(a);
+print(a, type(a), "\n");
+
+
+b = 1.2;
+print(b, type(b));
+b = str(b);
+print(b, type(b), "\n");
+
+
+c = "123";
+print(c, type(c));
+c = int(c);
+print(c, type(c), "\n");
+
+
+d = "123.123";
+print(d, type(d));
+d = float(d);
+print(d, type(d), "\n");
+
+
+e = 14.5;
+print(e, type(e));
+e = int(e);
+print(e, type(e), "\n");
+
+
+
+
+
+
+
+
+
+
+
