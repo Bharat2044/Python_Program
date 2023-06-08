5cb347b78e5624b3b8074828c7435ce0bff95b39
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:33 +0000

    code added

---
+++ 01_python_programs/13_conditional_statements.py
@@ -0,0 +1,34 @@
+x = 10;
+
+# if stetement
+if x > 5:
+    print("Hi\n");
+    
+
+# if else stetement
+if x > 20:
+    print("Hello\n");
+else:
+    print("Hola\n");
+    
+
+# if elif else stetement
+if x > 20:
+    print("Bharat\n");
+elif x > 15:
+    print("Deepak\n");
+elif x > 10:
+    print("Raju\n");
+else:
+    print("Mahi\n");
+
+
+
+
+
+
+
+
+
+
+
