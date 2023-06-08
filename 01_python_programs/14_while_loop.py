137a93f6ce2a917d3c6a305c19f6fee1c198c294
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:36 +0000

    code added

---
+++ 01_python_programs/14_while_loop.py
@@ -0,0 +1,15 @@
+counter = 0;
+while (counter < 3):
+    counter = counter + 1;
+    print("Bharat");
+    
+    
+print();
+
+    
+i = 0;
+while (i < 3):
+    i += 1;
+    print("Hello Ji");
+else:
+    print("In Else Block");
