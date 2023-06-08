c89e3aa9f7ab6b692d0c060cd3cea98c63f3e0c5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:50:49 +0000

    code added

---
+++ 01_python_programs/02_comments.py
@@ -0,0 +1,18 @@
+print("Single Line Comment");
+# This is single line comment
+
+
+print("Multi Line Comment");
+'''
+This 
+is
+    multi-line
+comments
+'''
+
+"""
+This 
+    is
+multi-line
+comments
+"""
