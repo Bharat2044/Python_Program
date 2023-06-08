50a0ed7860230d9b628cf9bdfe0b4c925040c0b1
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-08 16:51:22 +0000

    code added

---
+++ 01_python_programs/11_taking_input.py
@@ -0,0 +1,11 @@
+a = input("Please Enter the input: ");      # input()
+print(a, type(a), "\n");
+
+b = int(input("Please Enter the input: "));
+print(b, type(b), "\n");
+
+c = float(input("Please Enter the input: "));
+print(c, type(c), "\n");
+
+d = bool(input("Please Enter the input: "));
+print(d, type(d), "\n");
